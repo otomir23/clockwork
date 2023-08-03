524dd40a868b7292b14401d05b10cf3bca18cb86
Damir Modyarov <otomir@yandex.ru>
2023-08-03 22:13:54 +0300

    Add command aliases and an Easter egg

--- src/main/java/me/otomir23/clockwork/commands/ReconnectCommand.kt
+++ src/main/java/me/otomir23/clockwork/commands/ReconnectCommand.kt
@@ -5,6 +5,8 @@ import me.otomir23.clockwork.core.Reconnect
 import me.otomir23.clockwork.storage.Timing
 import me.otomir23.clockwork.storage.TimingsManager
 import net.minecraft.text.Text
+import net.silkmc.silk.commands.ClientCommandSourceStack
+import net.silkmc.silk.commands.LiteralCommandBuilder
 import net.silkmc.silk.commands.clientCommand
 import net.silkmc.silk.commands.sendSuccess
 import kotlin.math.roundToLong
@@ -15,18 +17,21 @@ object ReconnectCommand {
     private val UNKNOWN_TIMING_EXCEPTION = SimpleCommandExceptionType(Text.translatable("clockwork.commands.unknown_timing"))
 
     fun register() {
-        clientCommand("reconnect") {
+        val nodeTree: LiteralCommandBuilder<ClientCommandSourceStack>.() -> Unit = {
+            runs {
+                source.sendSuccess(Text.translatable("clockwork.commands.root.success"))
+            }
             literal("add") {
                 argument<String>("name") { name ->
                     argument<Float>("secondsDelay") { delay ->
                         runs {
                             if (delay() < 0) throw INVALID_DELAY_EXCEPTION.create()
                             if (TimingsManager.create(
-                                Timing(
-                                    name(),
-                                    (delay() * 1000L).roundToLong()
-                                )
-                            ))
+                                    Timing(
+                                        name(),
+                                        (delay() * 1000L).roundToLong()
+                                    )
+                                ))
                                 source.sendSuccess(Text.translatable("clockwork.commands.add.success"))
                             else
                                 throw ALREADY_ADDED_EXCEPTION.create()
@@ -56,5 +61,11 @@ object ReconnectCommand {
                 }
             }
         }
+
+        clientCommand("reconnect", builder = nodeTree)
+
+        clientCommand("timing", builder = nodeTree)
+
+        clientCommand("clockwork", builder = nodeTree)
     }
 }
