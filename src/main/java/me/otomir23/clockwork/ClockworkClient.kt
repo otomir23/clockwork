a422fc310615bdfc32bbb6b1b2a5691aee395ce6
Damir Modyarov <otomir@yandex.ru>
2023-08-04 13:08:02 +0300

    Fix limbo state when connection is not cleared

--- src/main/java/me/otomir23/clockwork/ClockworkClient.kt
+++ src/main/java/me/otomir23/clockwork/ClockworkClient.kt
@@ -4,12 +4,15 @@ import me.otomir23.clockwork.commands.ReconnectCommand
 import me.otomir23.clockwork.core.Reconnect
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents
+import org.slf4j.LoggerFactory
 
 object ClockworkClient : ClientModInitializer {
-    val MOD_ID = "clockwork"
+    const val MOD_ID = "clockwork"
+    val LOGGER = LoggerFactory.getLogger(javaClass)
 
     override fun onInitializeClient() {
         ClientPlayConnectionEvents.JOIN.register(Reconnect::onConnect)
+        ClientPlayConnectionEvents.DISCONNECT.register(Reconnect::onLeave)
         ReconnectCommand.register()
     }
 }
--- src/main/java/me/otomir23/clockwork/core/Reconnect.kt
+++ src/main/java/me/otomir23/clockwork/core/Reconnect.kt
@@ -1,5 +1,6 @@
 package me.otomir23.clockwork.core
 
+import me.otomir23.clockwork.ClockworkClient.LOGGER
 import net.fabricmc.fabric.api.networking.v1.PacketSender
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.network.ClientPlayNetworkHandler
@@ -23,9 +24,18 @@ object Reconnect {
         }
     }
 
+    fun onLeave(clientPlayNetworkHandler: ClientPlayNetworkHandler, minecraftClient: MinecraftClient) {
+        connection = null
+    }
+
     fun scheduleReconnect(delay: Long) {
         if (delay < 0) return
         connection?.let {
+            if (this.delay != null) {
+                LOGGER.warn("Task already scheduled, recreating Timer. This is not supposed to happen.")
+                timer.cancel()
+                timer = Timer()
+            }
             this.delay = delay
             it.connection.disconnect(Text.empty())
             timer.schedule(ReconnectTask(it), delay)
